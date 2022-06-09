3780b626cf385704bdd4d25bc750a750a6b915d6
rayan <87000151+rayanwaked@users.noreply.github.com>
2022-06-09 12:18:40 -0700

    dashboard-picker.css and dashboard-picker.tsx, added some UI elements.

--- src/components/global/navigation/dashboard-picker/dashboard-picker.tsx
+++ src/components/global/navigation/dashboard-picker/dashboard-picker.tsx
@@ -6,10 +6,18 @@ import "./dashboard-picker.css";
 
 /* Code */
 export default function DashboardPicker() {
-  return (
-    <div className="DashboardPicker">
-      <hr className="divider" />
-      <div className="DashboardPickerContainer"></div>
-    </div>
-  );
+    return (
+        <div className="DashboardPicker">
+            <hr className="divider" />
+            <div className="DashboardPickerContainer">
+                <div className="DashboardFragmentsSeparate">
+                    <div className="DashboardPickerFragments">
+                        <img src="https://images.unsplash.com/photo-1550537687-c91072c4792d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80"
+                             alt="Icon" />
+                        <p className="Header"> Procure </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
