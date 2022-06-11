e96defed35b0cf7c03a1c1482f53ff795319ae89
rayan <87000151+rayanwaked@users.noreply.github.com>
2022-06-10 21:24:13 -0700

    Element resizing

--- src/assets/dashboard/search.tsx
+++ src/assets/dashboard/search.tsx
@@ -1,8 +1,8 @@
 import * as React from "react";
 
 export const SearchIcon = ({
-                               height = "28px",
-                               width = "28px",
+                               height = "24px",
+                               width = "24px",
                                color = "white",
                                secondaryColor,
                                ...props
@@ -12,19 +12,24 @@ export const SearchIcon = ({
          viewBox="0 0 24 24"
          fill="none"
          xmlns="http://www.w3.org/2000/svg">
-        <g id="Iconly/Bulk/Search">
+        <g id="Iconly/Light/Search">
             <g id="Search">
-                <ellipse id="Ellipse_746"
-                         cx="10.5992"
-                         cy="10.6532"
-                         rx="8.59922"
-                         ry="8.65324"
-                         fill="white" />
-                <path id="Path_34202"
-                      opacity="0.4"
-                      d="M20.6746 21.9553C20.3406 21.9444 20.023 21.807 19.7854 21.5705L17.749 19.1901C17.3124 18.7909 17.2767 18.1123 17.6689 17.6689C17.8525 17.4831 18.1022 17.3786 18.3626 17.3786C18.6229 17.3786 18.8726 17.4831 19.0562 17.6689L21.6173 19.7181C21.9862 20.0957 22.1 20.6562 21.9079 21.1492C21.7158 21.6422 21.2536 21.9754 20.728 22L20.6746 21.9553Z"
-                      fill="white" />
+                <circle id="Ellipse_739"
+                        cx="11.7666"
+                        cy="11.7666"
+                        r="8.98856"
+                        stroke="white"
+                        stroke-width="1.5"
+                        stroke-linecap="round"
+                        stroke-linejoin="round" />
+                <path id="Line_181"
+                      d="M18.0183 18.4851L21.5423 22"
+                      stroke="white"
+                      stroke-width="1.5"
+                      stroke-linecap="round"
+                      stroke-linejoin="round" />
             </g>
         </g>
     </svg>
+
 );
