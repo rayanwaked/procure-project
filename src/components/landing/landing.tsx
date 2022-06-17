bbfd8dc5e19d667ab9ef52db5546795bca2d0941
rayan <87000151+rayanwaked@users.noreply.github.com>
2022-06-16 20:30:24 -0700

    Testamonial completed for both desktop and mobile. Removed dynamic sizing for text and button elements.

--- src/components/landing/landing.tsx
+++ src/components/landing/landing.tsx
@@ -28,7 +28,36 @@ export default function Landing() {
                         </button>
                     </div>
 
-                    <div className="LandingTestamonial"></div>
+                    <div className="LandingTestamonial">
+                        <div className="LandingTestamonialContent">
+
+                            <div>
+                                <img src="https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1528&q=80"
+                                     style={{
+                                         width: "8vh",
+                                         height: "8vh",
+                                         borderRadius: "100%",
+                                         objectFit: "cover",
+                                         marginTop: "20px",
+                                         marginLeft: "30px"
+                                     }} />
+                            </div>
+
+                            <div style={{display: "grid"}}>
+                                <p className="LandingTestamonialText">
+                                    “A testimonial that absolutely nails the value
+                                    proposition in a sentence or two”. </p>
+                                <p className="LandingTestamonialText"
+                                   style={{
+                                       fontSize: "13px",
+                                       marginTop: "-2px",
+                                       paddingBottom: "10px"
+                                   }}>
+                                    McKensey Woods, CEO, Mckensey & Co.</p>
+                            </div>
+
+                        </div>
+                    </div>
 
                 </div>
 
