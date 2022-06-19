5691bd59cd7043f3a94cabfe4aa8ae400d0be8b3
rayan <87000151+rayanwaked@users.noreply.github.com>
2022-06-18 19:59:59 -0700

    Finishing up refactoring of landing elements into seperate component directories

--- src/pages/landing/hero/history/history.tsx
+++ src/pages/landing/hero/history/history.tsx
@@ -7,8 +7,53 @@ import "./history.css";
 /* Code */
 export default function HeroHistory() {
     return (
-        <div className="Main">
-            <div className="Container"></div>
+        <div className="History">
+
+            {/* Payment History Title and Search Bar */}
+            <div className="HeroPaymentHistoryContent">
+                <p className="HeroPaymentHistoryLargeText">Order History</p>
+                <div className="HeroPaymentHistorySearchBar">
+                    <p className="HeroPaymentHistorySmallText" style={{marginLeft: "12px", color: "#6A7183"}}>Search</p>
+                </div>
+            </div>
+
+            {/* Payment History Content Labels */}
+            <div className="HeroPaymentHistoryContent" style={{marginTop: "-5px"}}>
+                <p className="HeroPaymentHistorySmallText" style={{fontSize: "14px", color: "#6A7183"}}>TRANSACTION ID</p>
+                <p className="HeroPaymentHistorySmallText" id="NoMobile" style={{fontSize: "14px", color: "#6A7183"}}>DATE</p>
+                <p className="HeroPaymentHistorySmallText" style={{fontSize: "14px", color: "#6A7183"}}>AMOUNT</p>
+                <p className="HeroPaymentHistorySmallText" id="NoMobile" style={{fontSize: "14px", color: "#6A7183"}}>STATUS</p>
+            </div>
+
+            {/* Payment History Content */}
+            <div className="HeroPaymentHistoryRow">
+
+                {/* Payment History Transaction ID */}
+                <div className="HeroPaymentHistory" style={{marginTop: "-5px"}}>
+                    <p className="HeroPaymentHistorySmallText" style={{fontSize: "14px", color: "white", whiteSpace: "nowrap"}}>99  99999 99752</p>
+                    <p className="HeroPaymentHistorySmallText" style={{fontSize: "14px", color: "white", whiteSpace: "nowrap"}}>99  99999 99753</p>
+                </div>
+
+                {/* Payment History Dates */}
+                <div className="HeroPaymentHistory" id="NoMobile" style={{marginTop: "-5px"}}>
+                    <p className="HeroPaymentHistorySmallText" style={{fontSize: "14px", color: "white"}}>10/12</p>
+                    <p className="HeroPaymentHistorySmallText" style={{fontSize: "14px", color: "white"}}>10/13</p>
+                </div>
+
+                {/* Payment History Amounts */}
+                <div className="HeroPaymentHistory" style={{marginTop: "-5px"}}>
+                    <p className="HeroPaymentHistorySmallText HeroPaymentHistoryMobile" style={{fontSize: "14px", color: "white", marginLeft: "6px"}}>$12,950</p>
+                    <p className="HeroPaymentHistorySmallText HeroPaymentHistoryMobile" style={{fontSize: "14px", color: "white", marginLeft: "6px"}}>$33,740</p>
+                </div>
+
+                {/* Payment History Statuses */}
+                <div className="HeroPaymentHistory" id="NoMobile" style={{marginTop: "-5px"}}>
+                    <p className="HeroPaymentHistorySmallText" id="HeroPaymentHistoryStatus">Posted</p>
+                    <p className="HeroPaymentHistorySmallText" id="HeroPaymentHistoryStatus" style={{marginTop: "19px"}}>Posted</p>
+                </div>
+
+            </div>
+
         </div>
     );
 }
--- src/pages/landing/landing.tsx
+++ src/pages/landing/landing.tsx
@@ -13,6 +13,7 @@ import Testimonial from "./testimonial/testimonial";
 import HeroProfile from "./hero/profile/profile";
 import HeroFinance from "./hero/finance/finance";
 import HeroInvoice from "./hero/invoice/invoice";
+import HeroHistory from "./hero/history/history";
 
 /* Code */
 export default function Landing() {
@@ -47,61 +48,14 @@ export default function Landing() {
                     <div className="HeroBackdropContent HeroAnimation"
                          id="HeroInvoicePaid"
                          style={{height: "11vw", minHeight: "151px"}}>
-
                         <HeroInvoice />
-
                     </div>
 
                     {/* Hero Payment History Card */}
                     <div className="HeroBackdropContent HeroAnimation"
                          id="HeroPaymentHistory"
                          style={{height: "15vw", minHeight: "205px", maxHeight: "210px", marginBottom: "-4vh"}}>
-
-                        {/* Payment History Title and Search Bar */}
-                        <div className="HeroPaymentHistoryContent">
-                            <p className="HeroPaymentHistoryLargeText">Order History</p>
-                            <div className="HeroPaymentHistorySearchBar">
-                                <p className="HeroPaymentHistorySmallText" style={{marginLeft: "12px", color: "#6A7183"}}>Search</p>
-                            </div>
-                        </div>
-
-                        {/* Payment History Content Labels */}
-                        <div className="HeroPaymentHistoryContent" style={{marginTop: "-5px"}}>
-                            <p className="HeroPaymentHistorySmallText" style={{fontSize: "14px", color: "#6A7183"}}>TRANSACTION ID</p>
-                            <p className="HeroPaymentHistorySmallText" id="NoMobile" style={{fontSize: "14px", color: "#6A7183"}}>DATE</p>
-                            <p className="HeroPaymentHistorySmallText" style={{fontSize: "14px", color: "#6A7183"}}>AMOUNT</p>
-                            <p className="HeroPaymentHistorySmallText" id="NoMobile" style={{fontSize: "14px", color: "#6A7183"}}>STATUS</p>
-                        </div>
-
-                        {/* Payment History Content */}
-                        <div className="HeroPaymentHistoryRow">
-
-                            {/* Payment History Transaction ID */}
-                            <div className="HeroPaymentHistory" style={{marginTop: "-5px"}}>
-                                <p className="HeroPaymentHistorySmallText" style={{fontSize: "14px", color: "white", whiteSpace: "nowrap"}}>99  99999 99752</p>
-                                <p className="HeroPaymentHistorySmallText" style={{fontSize: "14px", color: "white", whiteSpace: "nowrap"}}>99  99999 99753</p>
-                            </div>
-
-                            {/* Payment History Dates */}
-                            <div className="HeroPaymentHistory" id="NoMobile" style={{marginTop: "-5px"}}>
-                                <p className="HeroPaymentHistorySmallText" style={{fontSize: "14px", color: "white"}}>10/12</p>
-                                <p className="HeroPaymentHistorySmallText" style={{fontSize: "14px", color: "white"}}>10/13</p>
-                            </div>
-
-                            {/* Payment History Amounts */}
-                            <div className="HeroPaymentHistory" style={{marginTop: "-5px"}}>
-                                <p className="HeroPaymentHistorySmallText HeroPaymentHistoryMobile" style={{fontSize: "14px", color: "white", marginLeft: "6px"}}>$12,950</p>
-                                <p className="HeroPaymentHistorySmallText HeroPaymentHistoryMobile" style={{fontSize: "14px", color: "white", marginLeft: "6px"}}>$33,740</p>
-                            </div>
-
-                            {/* Payment History Statuses */}
-                            <div className="HeroPaymentHistory" id="NoMobile" style={{marginTop: "-5px"}}>
-                                <p className="HeroPaymentHistorySmallText" id="HeroPaymentHistoryStatus">Posted</p>
-                                <p className="HeroPaymentHistorySmallText" id="HeroPaymentHistoryStatus" style={{marginTop: "19px"}}>Posted</p>
-                            </div>
-
-                        </div>
-
+                        <HeroHistory />
                     </div>
                 </div>
             </div>
