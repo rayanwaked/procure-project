1bb0199a055bd8c6eba0d0e30f35f375204aac62
rayan <87000151+rayanwaked@users.noreply.github.com>
2022-06-23 17:58:42 -0700

    Footer alignment fixed

--- src/pages/landing/footer/footer.tsx
+++ src/pages/landing/footer/footer.tsx
@@ -7,38 +7,36 @@ import "./footer.css";
 /* Code */
 export default function LandingFooter() {
     return (
-        <div className="LandingFooterBackground">
-            <div className="LandingFooter">
-                <div className="LandingFooterSection">
-                    <h3 className="LandingFooterLogo">PROCURE</h3>
-                </div>
-                <div className="LandingFooterSection">
-                    <h3 className="LandingFooterHeadline">RESOURCES</h3>
-                    <a href={""}>
-                        <p className="LandingFooterLink">Contact</p></a>
-                    <a href={""}>
-                        <p className="LandingFooterLink">Blog</p></a>
-                    <a href={""}>
-                        <p className="LandingFooterLink">Help</p></a>
-                    <a href={""}>
-                        <p className="LandingFooterLink">Press</p></a>
-                </div>
-                <div className="LandingFooterSection">
-                    <h3 className="LandingFooterHeadline">PRODUCT</h3>
-                    <a href={""}>
-                        <p className="LandingFooterLink">Dashboard</p></a>
-                    <a href={""}>
-                        <p className="LandingFooterLink">Pricing</p></a>
-                    <a href={""}>
-                        <p className="LandingFooterLink">Inquiries</p></a>
-                </div>
-                <div className="LandingFooterSection">
-                    <h3 className="LandingFooterHeadline">COMPANY</h3>
-                    <a href={""}>
-                        <p className="LandingFooterLink">Mission</p></a>
-                    <a href={""}>
-                        <p className="LandingFooterLink">Careers</p></a>
-                </div>
+        <div className="LandingFooter">
+            <div className="LandingFooterSection">
+                <h3 className="LandingFooterLogo">PROCURE</h3>
+            </div>
+            <div className="LandingFooterSection">
+                <h3 className="LandingFooterHeadline">RESOURCES</h3>
+                <a href={""}>
+                    <p className="LandingFooterLink">Contact</p></a>
+                <a href={""}>
+                    <p className="LandingFooterLink">Blog</p></a>
+                <a href={""}>
+                    <p className="LandingFooterLink">Help</p></a>
+                <a href={""}>
+                    <p className="LandingFooterLink">Press</p></a>
+            </div>
+            <div className="LandingFooterSection">
+                <h3 className="LandingFooterHeadline">PRODUCT</h3>
+                <a href={""}>
+                    <p className="LandingFooterLink">Dashboard</p></a>
+                <a href={""}>
+                    <p className="LandingFooterLink">Pricing</p></a>
+                <a href={""}>
+                    <p className="LandingFooterLink">Inquiries</p></a>
+            </div>
+            <div className="LandingFooterSection">
+                <h3 className="LandingFooterHeadline">COMPANY</h3>
+                <a href={""}>
+                    <p className="LandingFooterLink">Mission</p></a>
+                <a href={""}>
+                    <p className="LandingFooterLink">Careers</p></a>
             </div>
         </div>
     );
