0f9291334f1b91776fa9d8411145ec25ad5a1781
rayan <87000151+rayanwaked@users.noreply.github.com>
2022-07-26 11:24:44 -0700

    Made most of the pricing css designs

--- src/pages/landing/pricing/pricing.tsx
+++ src/pages/landing/pricing/pricing.tsx
@@ -3,13 +3,52 @@ import "../../../styles.css";
 import "./pricing.css";
 
 /* Import Files */
-
+/* Import Icons */
 
 /* Code */
 export default function Pricing() {
     return (
-        <div className="">
-            <p> Test Pricing </p>
+        <div className="GettingStartedContainer">
+            <p className="LandingGettingStartedSubHeadline">SELECT YOUR PLAN</p>
+            <p className="LandingGettingStartedHeadline">Pricing</p>
+            <div className="LandingGettingStarted">
+                <div className={"PricingGettingStartedBox"}>
+                    <hr id={"pricingHR"} />
+                    <div className={"PricingTextBox"}>
+                        <p className="PricingText">Lorem ipsum dolor sit amet ipsum dolor sit</p>
+                        <p className="PricingText"
+                           style={{color: "#494f59", textDecoration: "line-through"}}>Lorem ipsum dolor sit amet ipsum
+                                                                                      dolor sit</p>
+                        <p className="PricingText"
+                           style={{color: "#494f59", textDecoration: "line-through"}}>Lorem ipsum dolor sit amet ipsum
+                                                                                      dolor sit</p>
+                    </div>
+                    <a href={""}
+                       className="PricingStepContainer">Choose Plan</a>
+                </div>
+                <div className={"PricingGettingStartedBox"}>
+                    <hr id={"pricingHR"} />
+                    <div className={"PricingTextBox"}>
+                        <p className="PricingText">Lorem ipsum dolor sit amet ipsum dolor sit</p>
+                        <p className="PricingText">Lorem ipsum dolor sit amet ipsum dolor sit</p>
+                        <p className="PricingText"
+                           style={{color: "#494f59", textDecoration: "line-through"}}>Lorem ipsum dolor sit amet ipsum
+                                                                                      dolor sit</p>
+                    </div>
+                    <a href={""}
+                       className="PricingStepContainer">Choose Plan</a>
+                </div>
+                <div className={"PricingGettingStartedBox"}>
+                    <hr id={"pricingHR"} />
+                    <div className={"PricingTextBox"}>
+                        <p className="PricingText">Lorem ipsum dolor sit amet ipsum dolor sit</p>
+                        <p className="PricingText">Lorem ipsum dolor sit amet ipsum dolor sit</p>
+                        <p className="PricingText">Lorem ipsum dolor sit amet ipsum dolor sit</p>
+                    </div>
+                    <a href={""}
+                       className="PricingStepContainer">Choose Plan</a>
+                </div>
+            </div>
         </div>
     );
 }
